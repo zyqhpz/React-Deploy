eb480ec7adb54a7487ac5121a5dd68634b11fd6f
zyqq0151 <haziq.hapiz@gmail.com>
2020-08-06 17:49:24 +0800

    Updates

---
+++ precache-manifest.3461579078d09cbe572d80c34810960e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "720c9bf2f73d138215cf810b8a9897af",
+    "url": "/React-Deploy/index.html"
+  },
+  {
+    "revision": "8a64b3ee9c0066ed9881",
+    "url": "/React-Deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "340aac5933c756125f93",
+    "url": "/React-Deploy/static/js/2.a0c31166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Deploy/static/js/2.a0c31166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a64b3ee9c0066ed9881",
+    "url": "/React-Deploy/static/js/main.7c2c92e6.chunk.js"
+  },
+  {
+    "revision": "be71b1056664f683eb46",
+    "url": "/React-Deploy/static/js/runtime-main.443423ab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
