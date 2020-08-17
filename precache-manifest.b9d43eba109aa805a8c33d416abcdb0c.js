707144e8baba1d789e64ef340c8996d18699fa2c
zyqq0151 <haziq.hapiz@gmail.com>
2020-08-17 16:40:09 +0800

    Updates

---
+++ precache-manifest.b9d43eba109aa805a8c33d416abcdb0c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a34a4dd829eb972ead832142e9c9859d",
+    "url": "/React-Deploy/index.html"
+  },
+  {
+    "revision": "b0729ed872c0d26f724b",
+    "url": "/React-Deploy/static/css/main.8a6cc2b7.chunk.css"
+  },
+  {
+    "revision": "ef4fa383d669fd91c7bc",
+    "url": "/React-Deploy/static/js/2.2c036b3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Deploy/static/js/2.2c036b3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0729ed872c0d26f724b",
+    "url": "/React-Deploy/static/js/main.21691223.chunk.js"
+  },
+  {
+    "revision": "be71b1056664f683eb46",
+    "url": "/React-Deploy/static/js/runtime-main.443423ab.js"
+  },
+  {
+    "revision": "7d9e8442432c6122b911433424fc95b0",
+    "url": "/React-Deploy/static/media/ig.7d9e8442.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Deploy/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a7a925c7c07f163f3274df0c1ab2c321",
+    "url": "/React-Deploy/static/media/twt.a7a925c7.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-Deploy/precache-manifest.22bfc2f18448fe31fd1260ad95c1da54.js"
+  "/React-Deploy/precache-manifest.b9d43eba109aa805a8c33d416abcdb0c.js"
 );
 
 self.addEventListener('message', (event) => {
